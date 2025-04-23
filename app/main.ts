6d4f714e84529f59c5ef44f4f19a98c55f909c65
Diego Sierra Fernandez <dsierra@theworkshop.com>
2025-04-23 16:59:05 +0200

    First commit

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,5 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
-import { Client } from "jsr:@bartlomieju/postgres";
+import { Client } from "jsr:@db/postgres";
 
 
 const router = new Router();
@@ -13,6 +13,7 @@ const client = new Client({
   hostname: "cluster.default.svc.cluster.local",
   port: 5432,
   tls: {
+    enforce: false,
     enable: false
   }
 });
