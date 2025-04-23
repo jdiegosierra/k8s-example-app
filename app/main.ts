7a05c3dde179ed363e564115be6f01c8c53acd1f
Diego Sierra Fernandez <dsierra@theworkshop.com>
2025-04-23 19:02:16 +0200

    First commit

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,4 @@
-console.log("Version 1.0.0")
+console.log("Version 1.0.1")
 
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 import { Client } from "https://deno.land/x/postgres/mod.ts";
